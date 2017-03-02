7b560ee58dca84c61ef2a747ac3d4b739b25d254
Liusea <vcrivenc@e1p95.chisinau.42.fr>
2017-03-02 17:01:13 +0200

    add header

---
+++ corewar.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   corewar.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcrivenc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/02 16:38:56 by vcrivenc          #+#    #+#             */
+/*   Updated: 2017/03/02 16:46:19 by vcrivenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COREWAR_H
+# define COREWAR_H
+
+typedef	struct	s_mat
+{
+	char	*laba;
+	char	*instr;
+	char	acb;
+	char	*arg1;
+	char	*arg2;
+	char	*arg3;
+}				t_mat;
+
+#endif
